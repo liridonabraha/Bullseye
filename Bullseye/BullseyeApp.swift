b92b37707599990687b8913a9c553afc6408d23d
Dom Bryan <dombguitar@gmail.com>
2022-03-14 15:18:27 +0000

    Initial Commit

---
+++ Bullseye/BullseyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BullseyeApp.swift
+//  Bullseye
+//
+//  Created by Dom Bryan on 14/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct BullseyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
