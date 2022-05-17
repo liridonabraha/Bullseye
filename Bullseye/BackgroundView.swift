b7bc7f368710344e1a3733cc514691e1f92c2ef8
Liridona <donabraha94@gmail.com>
2022-05-17 14:59:31 +0100

    R.W Bullseye project

---
+++ Bullseye/BackgroundView.swift
@@ -0,0 +1,105 @@
+//
+//  BackgroundView.swift
+//  Bullseye
+//
+//  Created by Dom Bryan on 11/04/2022.
+//
+
+import SwiftUI
+
+struct BackgroundView: View {
+    @Binding var game: Game
+    
+    var body: some View {
+        VStack {
+            TopView(game: $game)
+            Spacer()
+            BottomView(game: $game)
+        }
+        .padding()
+        .background(
+        RingsView()
+        )
+    }
+}
+
+///
+
+struct TopView: View {
+    @Binding var game: Game
+    @State private var leaderboardIsShowing = false
+    
+    var body: some View {
+        HStack {
+            Button(action: {
+                game.restart()
+            }) {
+                RoundedImageViewStroked(systemName: "arrow.counterclockwise")
+            }
+            Spacer()
+            Button(action: {
+                leaderboardIsShowing = true
+            }) {
+                RoundedImageViewStroked(systemName: "list.dash")
+            }.sheet(isPresented: $leaderboardIsShowing, onDismiss: {}, content: { LeaderboardView(leaderboardIsShowing: $leaderboardIsShowing, game: $game)
+                
+            })
+        }
+    }
+}
+
+///
+
+struct NumberVew: View {
+    var title: String
+    var text: String
+    
+    var body: some View {
+        VStack {
+            LabelText(text: title.uppercased())
+            RoundRectTextView(text: text)
+        }
+    }
+}
+
+///
+
+struct BottomView: View {
+    @Binding var game: Game
+    
+    var body: some View {
+        HStack {
+            NumberVew(title: "Score", text: String(game.score))
+            Spacer()
+            NumberVew(title: "Round", text: String(game.round))
+        }
+    }
+}
+
+struct RingsView: View {
+    
+    @Environment(\.colorScheme) var colorScheme
+    
+    var body: some View {
+        ZStack {
+            Color("BackgroundColor")
+                .edgesIgnoringSafeArea(.all)
+            ForEach(1..<6) { ring in
+                let size = CGFloat(ring * 100)
+                let opacity = colorScheme == .dark ? 0.1 : 0.3
+                Circle()
+                    .stroke(lineWidth: 20)
+                    .fill(
+                        RadialGradient(gradient: Gradient(colors: [Color("RingsColor").opacity(opacity), Color("RingsColor") .opacity(0)]), center: .center, startRadius: 100, endRadius: 300)
+                        )
+                    .frame(width: size, height: size)
+            }
+        }
+    }
+}
+
+struct BackgroundView_Previews: PreviewProvider {
+    static var previews: some View {
+        BackgroundView(game: .constant(Game()))
+    }
+}
